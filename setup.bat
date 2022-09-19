a541a966bdb1032d834f3c57db3145290195b1f3
Mattish <Mattish@Home>
2022-09-19 17:02:42 +0100

    Initial commit

---
+++ setup.bat
@@ -0,0 +1,3 @@
+pip install diffusers["training"]==0.3.0 transformers ftfy
+pip install "ipywidgets>=7,<8"
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
